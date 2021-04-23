d62bca931559d3f9f1455754a31947ed61fdc929
Artyom Pishalnikov <82143842+lemonmcline@users.noreply.github.com>
2021-04-23 12:29:00 +0600

    Arifmet-operation
    Arifmet-operation

---
+++ Arifmet operation.cpp
@@ -0,0 +1,50 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	int z = 228;
+	int x = 1337;
+	int c = 1337;
+	int y = 2;
+	int v;
+	v = z + y; 
+	cout <<v<< endl;
+
+	cout << endl;
+
+	cout << x - z << endl;
+
+	cout << endl;
+
+	cout << z * x << endl;
+
+	cout << endl;
+
+	cout << x / c << endl;
+
+	cout << endl;
+
+	cout << x % z << endl;
+
+	cout << endl;
+
+	y++;
+	cout << y << endl;
+
+	cout << endl;
+
+	y--;
+	cout << y << endl;
+
+	cout << endl;
+
+	int s230 = 4;
+	s230 += 6;
+	cout << s230 << endl;
+
+	cout << endl;
+
+	s230 = s230 + 10;
+	cout << s230 << endl;
+}
